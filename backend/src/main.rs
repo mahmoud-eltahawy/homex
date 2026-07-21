62f0e6d4783e38968c9e0acbad9385a7a913d6dd
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-07-21 16:21:31 +0300

    fix static file serving

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -54,7 +54,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     //SECTION : listener
     let addr = {
-        let host = env::var("HOMEX_HOST").unwrap_or_else(|_| "0.0.0.0".to_string());
+        let host = env::var("HOMEX_HOST").unwrap_or_else(|_| "127.0.0.1".to_string());
         let port = env::var("HOMEX_PORT").unwrap_or_else(|_| "3000".to_string());
 
         format!("{host}:{port}").parse::<SocketAddr>()?
@@ -130,8 +130,8 @@ fn static_file_handler() -> Router {
     #[cfg(not(debug_assertions))]
     let site_service = ServeDir::new("./").not_found_service(ServeFile::new("./index.html"));
     #[cfg(debug_assertions)]
-    let site_service = ServeDir::new("../homex/dist")
-        .not_found_service(ServeFile::new("../homex/dist/index.html"));
+    let site_service = ServeDir::new("../frontend/dist")
+        .not_found_service(ServeFile::new("../frontend/dist/index.html"));
     Router::new()
         .nest_service("/homex", public_dir)
         .fallback_service(site_service)
