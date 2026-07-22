0ca0e74c2e57efd99ec1660234e007269b78ecde
mahmoud-eltahawy <gemmy9300@gmail.com>
2026-07-23 02:06:16 +0300

    factor out resource view

--- src/app/series/details.rs
+++ src/app/series/details.rs
@@ -9,6 +9,7 @@ use crate::app::{
 };
 use leptos::{either::Either, prelude::*};
 use leptos_router::{hooks::use_params_map, lazy_route, LazyRoute};
+use serde::{de::DeserializeOwned, Serialize};
 
 pub struct SeriesDetailPage {
     pub series: Resource<Result<Series, ServerFnError>>,
@@ -36,29 +37,18 @@ impl LazyRoute for SeriesDetailPage {
     }
 
     fn view(this: Self) -> AnyView {
-        let fallback =
-            || view! { <div class="py-20 text-center text-white">"جارٍ التحميل..."</div> };
-
-        let refetch = move || this.series.refetch();
-
-        let core = move || match this.series.get() {
-            None => Either::Right(Either::Right(fallback())),
-            Some(Err(e)) => {
-                Either::Right(Either::Left(ServerFnErrorView(ServerFnErrorViewProps {
-                    e,
-                    refetch,
-                })))
-            }
-            Some(Ok(series)) => Either::Left(SeriesView(SeriesViewProps {
-                series,
-                episodes: this.episodes,
-                selected_season: this.selected_season,
-            })),
+        let adapter = move |series: Series| SeriesViewProps {
+            series,
+            episodes: this.episodes,
+            selected_season: this.selected_season,
         };
         view! {
-            <Suspense fallback=fallback>
-                {core}
-            </Suspense>
+            <ResourceView
+                resource=this.series
+                view_fn=SeriesView
+                adapter=adapter
+                context="تحميل الحلقات"
+            />
         }
         .into_any()
     }
@@ -154,34 +144,67 @@ fn Selectors(
     episodes: Resource<Result<Season, ServerFnError>>,
     summaries: Vec<SeasonSummary>,
 ) -> impl IntoView {
-    let fallback = || view! { <p class="text-gray-400">جارٍ تحميل الحلقات...</p> };
-
+    let adapter = move |season: Season| EpisodeSelectorProps {
+        episodes: season.episodes.clone(),
+        selected_episode,
+    };
     view! {
         <div class="mt-10">
             <SeasonSelector
                 summaries=summaries
                 selected_season=selected_season
             />
-            <Suspense fallback=fallback>
-                {move || match episodes.get() {
-                    None => Either::Left(Either::Right(fallback())),
-                    Some(Err(e)) => Either::Left(Either::Left(ServerFnErrorView(ServerFnErrorViewProps { e, refetch: move ||episodes.refetch()  }))),
-                    Some(Ok(season)) => Either::Right(
-                        EpisodeSelector(EpisodeSelectorProps { episodes:season.episodes.clone(), selected_episode })),
-                }}
-            </Suspense>
+            <ResourceView
+                resource=episodes
+                view_fn=EpisodeSelector
+                adapter=adapter
+                context="تحميل تلخيصات المواسم"
+            />
         </div>
     }
 }
 
 #[component]
-fn ServerFnErrorView<F>(e: ServerFnError, refetch: F) -> impl IntoView
+fn ResourceView<ViewFn, ResourceValue, ViewValue, Props, Adapter>(
+    resource: Resource<Result<ResourceValue, ServerFnError>>,
+    view_fn: ViewFn,
+    adapter: Adapter,
+    context: &'static str,
+) -> impl IntoView
+where
+    ResourceValue: Serialize + DeserializeOwned + Clone + Send + Sync + 'static,
+    ViewValue: IntoView + Send + 'static,
+    ViewFn: Send + Sync + 'static + Fn(Props) -> ViewValue,
+    Props: Send + Sync + 'static,
+    Adapter: Fn(ResourceValue) -> Props + Send + 'static,
+{
+    let fallback = move || Fallback(FallbackProps { context });
+
+    let core = move || match resource.get() {
+        None => Either::Left(Either::Right(fallback())),
+        Some(Err(e)) => Either::Left(Either::Left(ServerFnErrorView(ServerFnErrorViewProps {
+            e,
+            refetch: move || resource.refetch(),
+            context,
+        }))),
+        Some(Ok(val)) => Either::Right(view_fn(adapter(val))),
+    };
+
+    view! {
+        <Suspense fallback=fallback>
+            {core}
+        </Suspense>
+    }
+}
+
+#[component]
+fn ServerFnErrorView<F>(e: ServerFnError, refetch: F, context: &'static str) -> impl IntoView
 where
     F: Fn() + 'static,
 {
     view! {
         <div class="py-8 text-center">
-            <div class="text-red-400 text-sm font-bold mb-2">"حدث خطأ"</div>
+            <div class="text-red-400 text-sm font-bold mb-2">"حدث خطأ اثناء " {context}</div>
             <p class="text-gray-500 text-xs mb-3">{e.to_string()}</p>
             <button
                 on:click=move |_| refetch()
@@ -191,4 +214,9 @@ where
             </button>
         </div>
     }
+}
+
+#[component]
+fn Fallback(context: &'static str) -> impl IntoView {
+    view! { <p class="text-gray-400">"جارٍ ..." {context}</p> };
 }
